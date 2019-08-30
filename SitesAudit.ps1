915fb734b0edd39c2965685aa4836cd6465b5fcb
Chris Blackburn <thetootall@gmail.com>
2019-08-29 21:53:07 -0500

    Update SitesAudit.ps1
    Updated to use EXO MFA

--- SitesAudit.ps1
+++ SitesAudit.ps1
@@ -1,14 +1,14 @@
 #reference 
 #https://www.catapultsystems.com/blogs/pull-onedrive-for-business-usage-using-powershell/
 #https://www.jijitechnologies.com/blogs/how-to-get-the-storage-used-by-office365-groups
+#https://gallery.technet.microsoft.com/Connect-to-Exchange-Online-7d7365e0
 
 $value = get-module *sharepoint*
 if ($value -eq $null){
 Write-host "You do not have the Sharepoint online module installed; please wait"
 Start-process "https://www.microsoft.com/en-us/download/details.aspx?id=35588"
+Exit
 }
-Else
-{
 
 # Get values for input parameters:
 
@@ -21,8 +21,47 @@ Write-host "Connecting to Sharepoint Online Powershell, ensure module is install
 Connect-SPOService -Url $SPOAdminCenterUrl -Credential $credential
 
 Write-host "Connecting to Exchange Online Powershell" -BackgroundColor Black -ForegroundColor Yellow
-$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell -Credential $credential -Authentication Basic -AllowRedirection
-Import-PSSession $Session
+
+  $MFAExchangeModule = ((Get-ChildItem -Path $($env:LOCALAPPDATA+"\Apps\2.0\") -Filter CreateExoPSSession.ps1 -Recurse ).FullName | Select-Object -Last 1) 
+  If ($MFAExchangeModule -eq $null) 
+  { 
+   Write-Host "'nPlease install Exchange Online MFA Module" -ForegroundColor yellow 
+   Write-Host "You can install module using below blog : `nLink `nOR you can install module directly by entering "Y"`n:
+   $Confirm= "Read-Host "Are you sure you want to install module directly? [Y] Yes [N] No"
+   if($Confirm -match "[yY]") 
+   { 
+     Write-Host Yes 
+     Start-Process "iexplore.exe" "https://cmdletpswmodule.blob.core.windows.net/exopsmodule/Microsoft.Online.CSE.PSModule.Client.application" 
+   } 
+   else 
+   { 
+    Start-Process 'https://o365reports.com/2019/04/17/connect-exchange-online-using-mfa/' 
+    Exit 
+   } 
+   $Confirmation= Read-Host "Have you installed Exchange Online MFA Module? [Y] Yes [N] No"
+   if($Confirmation -match "[yY]") 
+   { 
+    $MFAExchangeModule = ((Get-ChildItem -Path $($env:LOCALAPPDATA+"\Apps\2.0\") -Filter CreateExoPSSession.ps1 -Recurse ).FullName | Select-Object -Last 1) 
+    If ($MFAExchangeModule -eq $null) 
+    { 
+     Write-Host "Exchange Online MFA module is not available" -ForegroundColor red 
+     Exit 
+    } 
+   } 
+   else 
+   {  
+    Write-Host Exchange Online PowerShell Module is required 
+    Start-Process 'https://o365reports.com/2019/04/17/connect-exchange-online-using-mfa/' 
+    Exit 
+   }    
+  } 
+   
+  #Importing Exchange MFA Module 
+  . "$MFAExchangeModule" 
+  Connect-EXOPSSession -WarningAction SilentlyContinue 
+
+#$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell -Credential $credential -Authentication Basic -AllowRedirection
+#Import-PSSession $Session
 
 # Getting Value from all Office 365 Groups
 
@@ -50,4 +89,3 @@ Write-host "Collection complete; please email " -BackgroundColor Black -Foregrou
 
 Get-PSSession | Remove-PSSession
 Disconnect-SPOService
-}
