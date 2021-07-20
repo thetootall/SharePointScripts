f5986c51d63d8473aade965131a718db4ab19135
Chris Blackburn <thetootall@gmail.com>
2021-07-20 17:49:08 -0500

    Update SitesAudit.ps1

--- SitesAudit.ps1
+++ SitesAudit.ps1
@@ -1,74 +1,64 @@
-#reference 
-#https://www.catapultsystems.com/blogs/pull-onedrive-for-business-usage-using-powershell/
-#https://www.jijitechnologies.com/blogs/how-to-get-the-storage-used-by-office365-groups
-#https://gallery.technet.microsoft.com/Connect-to-Exchange-Online-7d7365e0
 
-$value = get-module *sharepoint*
-if ($value -eq $null){
-Write-host "You do not have the Sharepoint online module installed; please wait"
-Start-process "https://www.microsoft.com/en-us/download/details.aspx?id=35588"
-Exit
-}
-
-# Get values for input parameters:
-
-$tenant = Read-host "Enter your onmicrosoft tenant name"
+#Set the SPO URL
+$tenant = Read-host "Enter your onmicrosoft tenant vanity name"
 $SPOAdminCenterUrl="https://$tenant-admin.sharepoint.com/"
 
-# Connect to SharePoint Online and Exchange Online 
-
-Write-host "Connecting to Sharepoint Online Powershell, ensure module is installed" -BackgroundColor Black -ForegroundColor Yellow
-Connect-SPOService -Url $SPOAdminCenterUrl -Credential $credential
-
-Write-host "Connecting to Exchange Online Powershell" -BackgroundColor Black -ForegroundColor Yellow
-
-  $MFAExchangeModule = ((Get-ChildItem -Path $($env:LOCALAPPDATA+"\Apps\2.0\") -Filter CreateExoPSSession.ps1 -Recurse ).FullName | Select-Object -Last 1) 
-  If ($MFAExchangeModule -eq $null) 
+#Check for SPO v2 module inatallation
+ $Module = Get-Module *sharepoint* -ListAvailable
+ if($Module.count -eq 0) 
+ { 
+  Write-Host "Sharepoint Online module is not available"  -ForegroundColor yellow  
+  $SPOConfirm= Read-Host "Do you want to install the SPO module? [Y] Yes [N] No"
+  if($SPOConfirm -match "[yY]") 
   { 
-   Write-Host "'nPlease install Exchange Online MFA Module" -ForegroundColor yellow 
-   Write-Host "You can install module using below blog : `nLink `nOR you can install module directly by entering "Y"`n:
-   $Confirm= "Read-Host "Are you sure you want to install module directly? [Y] Yes [N] No"
-   if($Confirm -match "[yY]") 
-   { 
-     Write-Host Yes 
-     Start-Process "iexplore.exe" "https://cmdletpswmodule.blob.core.windows.net/exopsmodule/Microsoft.Online.CSE.PSModule.Client.application" 
-   } 
-   else 
-   { 
-    Start-Process 'https://o365reports.com/2019/04/17/connect-exchange-online-using-mfa/' 
-    Exit 
-   } 
-   $Confirmation= Read-Host "Have you installed Exchange Online MFA Module? [Y] Yes [N] No"
-   if($Confirmation -match "[yY]") 
-   { 
-    $MFAExchangeModule = ((Get-ChildItem -Path $($env:LOCALAPPDATA+"\Apps\2.0\") -Filter CreateExoPSSession.ps1 -Recurse ).FullName | Select-Object -Last 1) 
-    If ($MFAExchangeModule -eq $null) 
-    { 
-     Write-Host "Exchange Online MFA module is not available" -ForegroundColor red 
-     Exit 
-    } 
-   } 
-   else 
-   {  
-    Write-Host Exchange Online PowerShell Module is required 
-    Start-Process 'https://o365reports.com/2019/04/17/connect-exchange-online-using-mfa/' 
-    Exit 
-   }    
+   Write-host "Installing Sharepoint Online PowerShell module"
+   Install-Module Microsoft.Online.SharePoint.PowerShell -Repository PSGallery -AllowClobber -Force
   } 
-   
-  #Importing Exchange MFA Module 
-  . "$MFAExchangeModule" 
-  Connect-EXOPSSession -WarningAction SilentlyContinue 
+  else 
+  { 
+   Write-Host "Sharepoint module is required to connect Exchange Online. Please install module using Install-Module ExchangeOnlineManagement cmdlet." 
+   Exit
+  }
+ } 
 
-#$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell -Credential $credential -Authentication Basic -AllowRedirection
-#Import-PSSession $Session
+#Check for EXO v2 module inatallation
+ $Module = Get-Module ExchangeOnlineManagement -ListAvailable
+ if($Module.count -eq 0) 
+ { 
+  Write-Host "Exchange Online PowerShell V2 module is not available"  -ForegroundColor yellow  
+  $EXOConfirm= Read-Host "Do you want to install the EXO module? [Y] Yes [N] No"
+  if($EXOConfirm -match "[yY]") 
+  { 
+   Write-host "Installing Exchange Online PowerShell module"
+   Install-Module ExchangeOnlineManagement -Repository PSGallery -AllowClobber -Force
+  } 
+  else 
+  { 
+   Write-Host "EXO V2 module is required to connect Exchange Online. Please install module using Install-Module ExchangeOnlineManagement cmdlet." 
+   Exit
+  }
+ } 
+
+#Connect to EXO & SPO Modules
+
+Try {
+    $TestEXO = (Get-OrganizationConfig).ExchangeVersion  }
+Catch
+     { Write-Host "Connecting to Exchange Online" -ForegroundColor Green
+Connect-ExchangeOnline }
+Try {
+    $TestSPO = (Get-SPOTenant).StorageQuota }
+Catch
+    { Write-Host "Connecting to SharePoint Online" -ForegroundColor Green
+Connect-SPOService -Url $SPOAdminCenterUrl -Credential $credential }
+
 
 # Getting Value from all Office 365 Groups
 
 $Groups = Get-UnifiedGroup
 $Groups | Foreach-Object{
 $Group = $_
-$site=Get-SPOSite -Identity $Group.SharePointSiteUrl -Detailed
+$site=Get-SPOSite -Identity ($Group).SharePointSiteUrl -Detailed
       New-Object -TypeName PSObject -Property @{
       GroupName=$site.Title
       CurrentStorageInMB=$site.StorageUsageCurrent
